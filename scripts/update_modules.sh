23cc61efad8bbc3c05c6c855048b8695a6affe2b
Maxim Kremenev <ezotrank@gmail.com>
2012-05-19 21:48:32 +0300

    Pulled submodules

--- scripts/update_modules.sh
+++ scripts/update_modules.sh
@@ -6,10 +6,13 @@ MODULES_DIR=$EMACS_HOME/git-modules
 for module in $(ls $MODULES_DIR); do
     cd $MODULES_DIR/$module
     echo "Start pull in $module"
+    git checkout master
     git pull
     if [[ -f Makefile ]]; then
-    	echo "Compile $module"
-    	make
+        echo "Compile $module"
+        make
     fi
 done
 
+cd $EMACS_HOME
+git commit -am 'Pulled submodules'
